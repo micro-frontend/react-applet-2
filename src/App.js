1fe00ca1b0d267d1967e0efc66c58617a2f54c57
Zhicheng Wang <zcwang@thoughtworks.com>
2018-10-09 17:04:22 +0800

    feat: 容器与 Applet 通讯

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Messenger from './Messenger';
+
+const messenger = new Messenger();
+window['messenger@$MFID$'] = messenger;
+
 class App extends Component {
   render() {
     return (
---
+++ src/Messenger.js
@@ -0,0 +1,7 @@
+class Messenger {
+  sayHello(message) {
+    alert(`${message} @ Applet 2`);
+  }
+}
+
+export default Messenger;
